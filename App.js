7d9c0f5c3f015ebe4750d9ae90780bbb2ac81405
Angel_Brown <arbrown110@gmail.com>
2021-05-06 22:48:50 -0400

    adding pop up alerts.

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Alert, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("Text clicked")
   return (
     <SafeAreaView style={styles.container}>
      <Text onPress={handlePress}>Enter</Text>
-      <TouchableOpacity onPress={() => console.log("Image being pressed")}>
+      <TouchableOpacity onPress={() => console.log('Image being pressed')}>
       <Image
         blurrRadius={5}
         fadeDuration={3000}
@@ -22,7 +22,10 @@ export default function App() {
       <Button 
         color="green"
         title="Press to continue" 
-        onPress={() => console.log("Button pushed")} />
+        onPress={() => Alert.Alert(" Title", "The message you want to put",[
+          {text: "Enter", onPress: () => console.log("Enter")},
+          {text: "Exit", onPress: () => console.log("Exit")},
+        ])} />
     </SafeAreaView>
   );
 }
