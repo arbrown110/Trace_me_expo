f9ab2b6ba35d341801b802e40f77fdac1d9c73eb
Angel_Brown <arbrown110@gmail.com>
2021-05-06 22:40:58 -0400

    adding a button to screen with color change.

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("Text clicked")
   return (
     <SafeAreaView style={styles.container}>
      <Text onPress={handlePress}>Enter</Text>
-     <Image
-      blurrRadius={5}
-      fadeDuration={3000}
+      <TouchableOpacity onPress={() => console.log("Image being pressed")}>
+      <Image
+        blurrRadius={5}
+        fadeDuration={3000}
 
-      source={{
-       width: 200,
-       height: 300,
-       uri: "http://picsum.photos/200/300",
-     }} 
-     />
+        source={{
+        width: 200,
+        height: 300,
+        uri: "http://picsum.photos/200/300",
+      }} 
+      />
+      </TouchableOpacity>
+      <Button 
+        color="green"
+        title="Press to continue" 
+        onPress={() => console.log("Button pushed")} />
     </SafeAreaView>
   );
 }
@@ -29,3 +35,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// Touchable Opacity , Touchable highlight, Touchablewithoutfeedback are options for on press of image. Also onPress and onLongPress.
